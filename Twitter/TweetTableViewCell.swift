620905aabcc7f42748fb3a61104b571942c8b2b1
t9guan <50217057+t9guan@users.noreply.github.com>
2020-02-12 18:49:07 -0800

    Initial Commit

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Tony Guan on 2/12/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tweetLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
